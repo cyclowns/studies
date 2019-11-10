ad141826f74a66d0525b6f6f673d0030641cc308
cyclowns <notzombiedude@gmail.com>
2019-11-09 18:49:53 -0700

    dyn. programming rust example w/ lucas nums

---
+++ dynamic_programming/rust-lucas/src/main.rs
@@ -0,0 +1,32 @@
+use std::time::{Instant};
+
+fn main() {
+    let lr = Instant::now();
+    println!("Recurse @ 30: {}", lucas_recurse(30));
+    println!("Time taken: {}", lr.elapsed().as_micros());
+
+    let ldp = Instant::now();
+    println!("Dynamic prog. @ 30: {}", lucas_dp(30));
+    println!("Time taken: {}", ldp.elapsed().as_micros());
+}
+
+fn lucas_recurse(n: usize) -> usize {
+    match n {
+        0 => 2,
+        1 => 1,
+        n => {
+            lucas_recurse(n-2) + lucas_recurse(n-1)
+        }
+    }
+}
+
+fn lucas_dp(n: usize) -> usize {
+    let mut lookup_table: Vec<usize> = vec![0; n + 1]; 
+    lookup_table[0] = 2;
+    lookup_table[1] = 1;
+
+    for i in 2..(n + 1) {
+        lookup_table[i] = lookup_table[i-2] + lookup_table[i-1];
+    }
+    lookup_table[n]
+}
