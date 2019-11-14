fcfb628b7ab04ba63cdcba604a1808ade85099c3
cyclowns <notzombiedude@gmail.com>
2019-11-13 19:27:19 -0700

    insert sort

--- dynamic_programming/rust-lucas/src/main.rs
+++ dynamic_programming/rust-lucas/src/main.rs
@@ -2,11 +2,11 @@ use std::time::{Instant};
 
 fn main() {
     let lr = Instant::now();
-    println!("Recurse @ 30: {}", lucas_recurse(30));
+    println!("Recurse @ 30: {}", lucas_recurse(20));
     println!("Time taken: {}", lr.elapsed().as_micros());
 
     let ldp = Instant::now();
-    println!("Dynamic prog. @ 30: {}", lucas_dp(30));
+    println!("Dynamic prog. @ 30: {}", lucas_dp(20));
     println!("Time taken: {}", ldp.elapsed().as_micros());
 }
 
@@ -21,12 +21,14 @@ fn lucas_recurse(n: usize) -> usize {
 }
 
 fn lucas_dp(n: usize) -> usize {
-    let mut lookup_table: Vec<usize> = vec![0; n + 1]; 
+    let mut lookup_table: Vec<usize> = vec![0; 2]; 
     lookup_table[0] = 2;
     lookup_table[1] = 1;
 
-    for i in 2..(n + 1) {
-        lookup_table[i] = lookup_table[i-2] + lookup_table[i-1];
+    for i in 0..(n - 1) {
+        let temp = lookup_table[0];
+        lookup_table[0] = lookup_table[1];
+        lookup_table[1] = lookup_table[1] + temp;
     }
-    lookup_table[n]
+    lookup_table[1]
 }
--- sorting/rust-sorts/src/main.rs
+++ sorting/rust-sorts/src/main.rs
@@ -2,8 +2,9 @@
 mod tests;
 
 fn main() {
-    let example_vec = vec!['e', 'd', 'c', 'b', 'a'];
-    println!("{:?}", selection_sort(&example_vec));
+    let example_vec = vec!['2', '1', '3', '5', '4'];
+    println!("sel: {:?}", selection_sort(&example_vec));
+    println!("ins: {:?}", insertion_sort(&example_vec));
 }
 
 // selection sort
@@ -29,5 +30,25 @@ fn selection_sort<T: Ord + Copy>(array: &[T]) -> Vec<T> {
         sorted_vec.swap(i, min_subarray_index);
     }
 
+    sorted_vec
+}
+
+// insertion sort
+fn insertion_sort<T: Ord + Copy>(array: &[T]) -> Vec<T> {
+    let mut sorted_vec = array.clone().to_vec();
+    let len = sorted_vec.len();
+
+    for i in 1..len {
+        let mut j = i - 1;
+
+        while sorted_vec[j + 1] < sorted_vec[j] {
+            sorted_vec.swap(j + 1, j);
+            if j != 0 { 
+                j -= 1 
+            } else {
+                break;
+            }
+        }
+    }
     sorted_vec
 }
